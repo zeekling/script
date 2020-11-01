56711f5fa5cb26e61c9c1e27d9af55efe80da8aa
zeekling <lingzhaohui@zeekling.cn>
2020-11-01 16:34:43 +0800

    gitea最新版本修改配置路径

--- docker/gitea.sh
+++ docker/gitea.sh
@@ -10,6 +10,10 @@ docker run -d --name=gitea  \
 	--link mysql:mysql \
 	--link redis:redis \
 	-v /dockerData/gitea:/data \
+	-v /dockerData/gitea/gitea/:/var/lib/gitea/custom \
+	-v /dockerData/gitea/gitea/conf:/etc/gitea/ \
 	gitea/gitea:${gitea_version}
 	#-v /dockerData/gitea-ssh/sshd_config:/etc/ssh/sshd_config \
 	#01ba755800a2
+	#--link redis:redis \
+	#-v /dockerData/gitea:/var/lib/gitea \
