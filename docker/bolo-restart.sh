534de0d657d9740977a17d82e37c09deb0dcdb00
zeekling <lingzhaohui@zeekling.cn>
2020-11-01 17:46:40 +0800

    修改博客内存大小

--- docker/bolo-restart.sh
+++ docker/bolo-restart.sh
@@ -10,18 +10,16 @@ docker stop bolo
 docker rm bolo
 docker run --detach --name bolo --network=host  \
     --env RUNTIME_DB="MYSQL" \
-	--env JAVA_OPTS="-Xms265m -Xmx265m -Xmn170m -Xloggc:/var/log/gc.log" \
+	--env JAVA_OPTS="-Xloggc:/var/log/gc.log" \
     --env JDBC_USERNAME="root" \
-    --env JDBC_PASSWORD="123456" \
+    --env JDBC_PASSWORD="94adg2011" \
     --env JDBC_DRIVER="com.mysql.cj.jdbc.Driver" \
 	--env SERVER_HOST="www.zeekling.cn" \
 	--env SERVER_PORT="443" \
 	--env SERVER_SCHEME="https" \
     --env JDBC_URL="jdbc:mysql://127.0.0.1:3306/solo?useUnicode=yes&characterEncoding=UTF-8&allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC" \
     -v /dockerData/bolo/bolo-fantastic:/opt/bolo/skins/bolo-fantastic \
-    -v /dockerData/bolo/bolo-NeoEase:/opt/bolo/skins//bolo-NeoEase-mod \
-    zeek/bolo \
-	--lute_http=http://127.0.0.1:8249
+    zeek/bolo
 # zeek/bolo --listen_port=8080 --server_scheme=https --server_host=www.zeekling.cn \
 
     #b3log/solo --listen_port=8080 --server_scheme=https --server_host=www.zeekling.cn \
