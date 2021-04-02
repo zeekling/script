0226c0b1269642aa6f24ba12e28fa1ade390de1c
zeekling <lingzhaohui@zeekling.cn>
2021-04-02 21:37:01 +0800

    bolo start 脚本

--- docker/bolo-restart.sh
+++ docker/bolo-restart.sh
@@ -10,17 +10,14 @@ docker stop bolo
 docker rm bolo
 docker run --detach --name bolo --network=host  \
     --env RUNTIME_DB="MYSQL" \
-	--env JAVA_OPTS="-Xloggc:/var/log/gc.log" \
     --env JDBC_USERNAME="root" \
     --env JDBC_PASSWORD="94adg2011" \
     --env JDBC_DRIVER="com.mysql.cj.jdbc.Driver" \
 	--env SERVER_HOST="www.zeekling.cn" \
 	--env SERVER_PORT="443" \
-	--env SERVER_SCHEME="https" \
     --env JDBC_URL="jdbc:mysql://127.0.0.1:3306/solo?useUnicode=yes&characterEncoding=UTF-8&allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC" \
     -v /dockerData/bolo/bolo-fantastic:/opt/bolo/skins/bolo-fantastic \
-    zeek/bolo
-# zeek/bolo --listen_port=8080 --server_scheme=https --server_host=www.zeekling.cn \
+    zeek/bolo --listen_port=8080 --server_scheme=https --server_host=www.zeekling.cn --lute_http=http://127.0.0.1:8249 \
 
     #b3log/solo --listen_port=8080 --server_scheme=https --server_host=www.zeekling.cn \
 ##--env JDBC_URL="jdbc:mysql://172.27.0.17:3306/solo?useUnicode=yes&characterEncoding=UTF-8&useSSL=false&serverTimezone=UTC" \
